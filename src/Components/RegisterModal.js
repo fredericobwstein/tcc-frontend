607b42aabc7ab93e3c1c2afebd98368a1d90d772
Fred <fredberstein@gmail.com>
2024-07-16 13:57:42 -0300

    fix msg retorno

--- src/Components/RegisterModal.js
+++ src/Components/RegisterModal.js
@@ -67,7 +67,8 @@ export const RegisterModal = ({ isOpen, onRequestClose, onRegister }) => {
             <input type="password" id="senha" value={senha} onChange={(e) => setSenha(e.target.value)} required />
           </div>
           <button type="submit">Register</button>
-          {error && <p style={{ color: error.includes('sucess') ? 'green' : 'red' }}>{error}</p>}
+          {error &&             <p className={error === 'Register successfully!' ? 'success-message' : 'error-message'}>
+          {error}</p>}
           </form>
         <button onClick={onRequestCloseHandler} className="modal-close-button">Close</button>
         </div>
